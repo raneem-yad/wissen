9f965397d619bc876e7723747ce357afcd751c15
raneem-yad <eng.raneem.yad@gmail.com>
2024-05-09 12:49:40 +0200

    testing that react bootstrap works

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Button variant='primary'>Hello</Button>
       <h1>Hello!!!</h1>
     </div>
   );
